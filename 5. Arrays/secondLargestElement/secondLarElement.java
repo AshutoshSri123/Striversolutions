7a53f9a4060fb7a2d8cb7381bbc98f384b596409
AshutoshSri123 <ashu.61003@gmail.com>
2025-08-27 02:48:07 +0530

    removing wrong code

---
+++ 5. Arrays/secondLargestElement/secondLarElement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class secondLarElement{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        for(int i=0; i<a; i++){
+            arr[i] = sc.nextInt();
+        }
+        getSecLar(arr);
+    }
+    static int getSecLar(int[] array){
+
+    }
+
+}
