0e4588412ac46d3b64dead53a1cbe130437778d6
AshutoshSri123 <ashu.61003@gmail.com>
2025-09-04 17:35:58 +0530

    GCD code refine

--- 1. Lec four/findGCD/findGCD.java
+++ 1. Lec four/findGCD/findGCD.java
@@ -7,6 +7,18 @@ class findGCD{
         System.out.println(gcd(a,b));
     }
     static int gcd(int a, int b){
+        while(a>0 && b>0){
+            if(a>b){
+                a=a%b;
+            }else{
+                b=b%a;
+            }
+        }
+
+        if(a==0){
+            return b;
+        }
+        return a;
 
     }
 }
